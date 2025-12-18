78b8749e85a07ccf2431a5b891a07a112394b6b3
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2025-12-18 10:50:50 +0600

    comment

--- src/modules/opener/socialOpener.js
+++ src/modules/opener/socialOpener.js
@@ -1,136 +1,32 @@
-//w: ╭──────────── Block Start ────────────╮
-
-//w: ╰───────────── Block End ─────────────╯
-
-//w: ╭──────────── Block Start ────────────╮
-// p: open discord session announcement
-api.mapkey("ouc", "open gContacts", function () {
-  if (window.location.hostname.includes("contacts")) {
-    window.location.href = "https://contacts.google.com/new";
-  } else {
-    window.location.href = "https://contacts.google.com/";
-    window.open("https://contacts.google.com/new", "_blank");
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-
-//
-//w: ╭──────────── Block Start ────────────╮
-api.mapkey("oux", "open twitter @shahjalal_labs", function () {
-  if (window.location.hostname.includes("x.com")) {
-    window.location.href = "https://x.com/shahjalal_labs ";
-  } else {
-    window.open("https://x.com/shahjalal_labs ", "_blank");
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-//
-
-//
-//w: ╭──────────── Block Start ────────────╮
-//t: facebook
+//* ╭──────────── Block Start ────────────╮
 api.mapkey("ouf", "Open Facebook", function () {
   window.open("https://www.facebook.com", "_blank");
 });
-//w: ╰───────────── Block End ─────────────╯
+//* ╰───────────── Block End ─────────────╯
 
-//
-//w: ╭──────────── Block Start ────────────╮
+//* ╭──────────── Block Start ────────────╮
 api.mapkey("oue", "email inbox opening", function () {
   window.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox", "_blank");
 });
-//w: ╰───────────── Block End ─────────────╯
+//* ╰───────────── Block End ─────────────╯
 
-//
-//w: ╭──────────── Block Start ────────────╮
-//t: open whatsApp
+//* ╭──────────── Block Start ────────────╮
 api.mapkey("ouw", "whatsApp", function () {
   window.open("https://web.whatsapp.com/", "_blank");
 });
-//w: ╰───────────── Block End ─────────────╯
+//* ╰───────────── Block End ─────────────╯
 
-//
-//w: ╭──────────── Block Start ────────────╮
-// p: open zoom session
+//* ╭──────────── Block Start ────────────╮
 api.mapkey("oul", "open linkedin", function () {
   if (window.location.hostname.includes("linkedin")) {
-    window.location.href = "https://www.linkedin.com/in/shahjalal-labs/";
+    window.location.href = "https://www.linkedin.com/in/ruhulamin-programming/";
   } else {
-    window.open("https://www.linkedin.com/in/shahjalal-labs/", "_blank");
+    window.open("https://www.linkedin.com/in/ruhulamin-programming/", "_blank");
   }
 });
-//w: ╰───────────── Block End ─────────────╯
+//* ╰───────────── Block End ─────────────╯
 
-//
-//w: ╭──────────── Block Start ────────────╮
-// p: open discord session announcement
-api.mapkey("oud", "open ejp discord sesssion announcement", function () {
-  if (window.location.hostname.includes("discord")) {
-    window.location.href =
-      "https://discord.com/channels/1391805056957026364/1391809512100462592";
-  } else {
-    window.open(
-      "https://discord.com/channels/1391805056957026364/1391809512100462592",
-      "_blank",
-    );
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-
-//
-//w: ╭──────────── Block Start ────────────╮
-// p: open zoom session
-api.mapkey("anz", "open ejp discord sesssion announcement", function () {
-  if (window.location.hostname.includes("zoom")) {
-    window.location.href = "https://www.zoom.us/join";
-  } else {
-    window.open("https://www.zoom.us/profile", "_blank");
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-
-//
-//w: ╭──────────── Block Start ────────────╮
-//t: open youtube @shahjalal-labs
-api.mapkey("ouy", "open yt @shahjalal-labs", function () {
-  if (window.location.hostname.includes("youtube")) {
-    window.location.href = "https://www.youtube.com/@shahjalal-labs";
-  } else {
-    window.open("https://www.youtube.com/@shahjalal-labs", "_blank");
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-
-//
-//w: ╭──────────── Block Start ────────────╮
-//t: open portfolio shahjalal-labs
-api.mapkey("oup", "open portfolio shahjalal-labs", function () {
-  if (window.location.hostname.includes("shahjalal-labs.surge.sh")) {
-    window.location.href = "https://shahjalal-labs.surge.sh";
-  } else {
-    window.open("https://shahjalal-labs.surge.sh", "_blank");
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-//
-//
-//w: ╭──────────── Block Start ────────────╮
-//t: open resume  shahjalal-labs
-api.mapkey("our", "open resume root", function () {
-  if (window.location.hostname.includes("drive.google.com")) {
-    window.location.href =
-      "https://drive.google.com/file/d/1eKx1OO7p2tvSIxPKD1aDHu6sgPkVcJwN/view";
-  } else {
-    window.open(
-      "https://drive.google.com/file/d/1eKx1OO7p2tvSIxPKD1aDHu6sgPkVcJwN/view",
-      "_blank",
-    );
-  }
-});
-//w: ╰───────────── Block End ─────────────╯
-
-//w: ╭──────────── Block Start ────────────╮
-// open ms team live
+//* ╭──────────── Block Start ────────────╮
 api.mapkey("out", "open ms team live", function () {
   if (window.location.hostname.includes("teams.live.com")) {
     window.location.href = "https://teams.live.com/v2/";
@@ -138,16 +34,4 @@ api.mapkey("out", "open ms team live", function () {
     window.open("https://teams.live.com/v2/", "_blank");
   }
 });
-//w: ╰───────────── Block End ─────────────╯
-//w: ╭──────────── Block Start ────────────╮
-
-//w: ╰───────────── Block End ─────────────╯
-//w: ╭──────────── Block Start ────────────╮
-
-//w: ╰───────────── Block End ─────────────╯
-//w: ╭──────────── Block Start ────────────╮
-
-//w: ╰───────────── Block End ─────────────╯
-//w: ╭──────────── Block Start ────────────╮
-
-//w: ╰───────────── Block End ─────────────╯
+//* ╰───────────── Block End ─────────────╯
