f42b3f9c9851c1586d58b1ea10baf9d28c209b90
Ruhul Amin <ruhulamin.cse56@gmail.com>
2025-12-19 09:13:13 +0600

    update db

--- src/modules/opener/opener.js
+++ src/modules/opener/opener.js
@@ -13,5 +13,8 @@ api.mapkey("ay", "youtube opening", function () {
 
 //* opening database
 api.mapkey("od", "Open Database", function () {
-  window.open("https://cloud.mongodb.com/v2/", "_blank");
+  window.open(
+    "https://cloud.mongodb.com/v2/689b5341cb09401668edf04d#/explorer/689b53cb74d9477bcbecb811",
+    "_blank"
+  );
 });
--- surfingkeys.bundle.js
+++ surfingkeys.bundle.js
@@ -397,7 +397,10 @@
     window.open("https://www.youtube.com/", "_blank");
   });
   api.mapkey("od", "Open Database", function() {
-    window.open("https://cloud.mongodb.com/v2/", "_blank");
+    window.open(
+      "https://cloud.mongodb.com/v2/689b5341cb09401668edf04d#/explorer/689b53cb74d9477bcbecb811",
+      "_blank"
+    );
   });
 
   // src/modules/opener/aiOpener.js
