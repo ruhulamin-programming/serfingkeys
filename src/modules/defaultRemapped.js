0ad1ec5ab26a964254458b9d13624d33b0e93af6
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2026-01-05 15:44:36 +0600

    update

--- src/modules/defaultRemapped.js
+++ src/modules/defaultRemapped.js
@@ -10,14 +10,10 @@ api.map("<Alt-j>", "l");
 api.map("<Alt-k>", "h");
 api.map("h", "E");
 api.map("gj", "G");
-api.map("gi", "yy", /.*youtube.*/i);
-//api.map("t", "f");
-//api.map("f", "t");
 api.map("l", "R");
 api.lmap("f", "<t>");
 api.map("as", ";fs");
-api.map("u", "<Ctrl-i>");
-api.map("Ctrl+d", "<Ctrl-f>");
+
 // api.map("<Ctrl-i>", "<Alt-s>"); // hotkey must be one keystroke with/without modifier, it can not be a sequence of keystrokes like `gg`.
 // an example to remove mapkey `Ctrl-i`
 api.unmap("<ctrl-i>");
