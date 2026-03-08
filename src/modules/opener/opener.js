f22fb7cdb6b0d67998db3263be4bfd8c60079496
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2026-03-08 13:44:08 +0600

    add

--- src/modules/opener/opener.js
+++ src/modules/opener/opener.js
@@ -2,3 +2,15 @@
 api.mapkey("ay", "youtube opening", function () {
   window.open("https://www.youtube.com/", "_blank");
 });
+
+api.mapkey("ogt", "open google translate", function () {
+  if (window.location.hostname.includes("translate.google")) {
+    window.location.href =
+      "https://translate.google.com/?sl=en&tl=bn&op=translate";
+  } else {
+    window.open(
+      "https://translate.google.com/?sl=en&tl=bn&op=translate",
+      "_blank",
+    );
+  }
+});
