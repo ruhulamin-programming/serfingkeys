18a4c019a4702dbdac79cb424dfca69bcc6ea64f
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2026-03-08 13:46:40 +0600

    update

--- surfingkeys.bundle.js
+++ surfingkeys.bundle.js
@@ -387,7 +387,7 @@
   api.mapkey("ay", "youtube opening", function() {
     window.open("https://www.youtube.com/", "_blank");
   });
-  api.mapkey("ogt", "open google translate", function() {
+  api.mapkey("ot", "open google translate", function() {
     if (window.location.hostname.includes("translate.google")) {
       window.location.href = "https://translate.google.com/?sl=en&tl=bn&op=translate";
     } else {
